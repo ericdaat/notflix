448a70f8938778758d0e35dd9d2949980e98974a
Eric Daoud <daoudattoyan.eric@gmail.com>
2018-11-04 18:20:57 +0100

    [FRONT] Add badges for genres

--- src/main.py
+++ src/main.py
@@ -14,9 +14,9 @@ if __name__ == "__main__":
                                 "display_name": "Similar to {0}",
                                 "priority": 0}))
 
-    # insert_in_db(EngineTable(**{"type": "MLBasedEngine",
-    #                             "display_name": "Machine Learning says this",
-    #                             "priority": 1}))
+    insert_in_db(EngineTable(**{"type": "MLBasedEngine",
+                                "display_name": "You might also like",
+                                "priority": 1}))
 
     r = Recommender()
     c = Context(item_id=100, user_id=3)
