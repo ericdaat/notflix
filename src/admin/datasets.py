48867b223657a8da12d97e6a0a854f3f73eb4d82
Eric Daoud <daoudattoyan.eric@gmail.com>
2018-12-06 11:18:28 +0100

    Fix dataset maker

--- src/admin/datasets.py
+++ src/admin/datasets.py
@@ -3,12 +3,13 @@ from data.downloader import OMDBDownloader
 import logging
 
 
-def make_movielens():
+def make_movielens(with_download=False):
     url = "http://www.omdbapi.com/"
     d = OMDBDownloader("admin/omdb.key", url)
 
-    d.file_to_items_from_api(input_filepath="data/datasets/movielens/ml-20m/links.csv",
-                             output_filepath="data/datasets/movielens/omdb.csv")
+    if with_download:
+        d.file_to_items_from_api(input_filepath="data/datasets/movielens/ml-20m/links.csv",
+                                 output_filepath="data/datasets/movielens/omdb.csv")
 
     d.insert_from_file_to_db(input_filepath="data/datasets/movielens/omdb.csv")
 
