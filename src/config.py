85f361aabdac3cfc544b7a7d58668dc49a66b05f
Eric Daoud <daoudattoyan.eric@gmail.com>
2019-02-03 21:18:30 +0100

    Moved DB_HOST in config.py

--- src/config.py
+++ src/config.py
@@ -4,5 +4,13 @@ import os
 CACHE_HOST = "redis"
 CACHE_TIMEOUT = 30
 
+# data.db
+DB_HOST = "postgresql://{user}:{password}@{host}/{db}".format(
+    user=os.environ["POSTGRES_USER"],
+    password=os.environ["POSTGRES_PASSWORD"],
+    db=os.environ["POSTGRES_DB"],
+    host=os.environ["HOST"]
+)
+
 # recommender
 MAX_RECOMMENDATIONS = 25
--- src/data/db/__init__.py
+++ src/data/db/__init__.py
@@ -1,15 +1,7 @@
-import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
-
-DB_HOST = "postgresql://{user}:{password}@{host}/{db}".format(
-    user=os.environ["POSTGRES_USER"],
-    password=os.environ["POSTGRES_PASSWORD"],
-    db=os.environ["POSTGRES_DB"],
-    host=os.environ["HOST"]
-)
+from config import DB_HOST
 
 engine = create_engine(DB_HOST, convert_unicode=True)
 
