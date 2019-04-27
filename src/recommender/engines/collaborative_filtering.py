0d30d74fd7dc6134f913135248ac1040dd7283ae
Eric <daoudattoyan.eric@gmail.com>
2019-04-27 17:35:32 +0200

    add Item2Vec

--- src/recommender/engines/collaborative_filtering.py
+++ src/recommender/engines/collaborative_filtering.py
@@ -3,6 +3,7 @@ import pandas as pd
 import logging
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import pairwise_distances
+from gensim.models import Word2Vec
 
 from src.recommender.engines import OfflineEngine
 from src.utils.data import (
@@ -36,5 +37,55 @@ class ItemBasedCF(OfflineEngine):
             n_recommendations=MAX_RECOMMENDATIONS,
             input_kind=self.input_id_kind
         )
+
+        self.save_recommendations_to_csv(recommendations)
+
+
+class Item2Vec(OfflineEngine):
+    def __init__(self):
+        super(Item2Vec, self).__init__()
+        self.input_id_kind = "item"
+
+    def train(self):
+        logging.info("training {0}".format(self.type))
+
+        df = pd.read_csv(
+            os.path.join(DATASETS_PATH, "movielens", "ml-1m", "ratings.csv")
+        )
+
+        corpus = df[["userId", "movieId"]]\
+            .astype(str)\
+            .groupby("userId")["movieId"]\
+            .apply(list).tolist()
+
+        model = Word2Vec(
+            min_count=0,
+            window=10,
+            size=100
+        )
+
+        model.build_vocab(corpus)
+
+        model.train(
+            corpus,
+            total_examples=model.corpus_count,
+            epochs=20
+        )
+
+        recommendations = []
+
+        for movie_id in model.wv.index2word:
+            neighbors = model.wv.most_similar(
+                movie_id,
+                topn=MAX_RECOMMENDATIONS
+            )
+
+            for neighbor_movie_id, score in neighbors:
+                recommendations.append([
+                    int(movie_id),
+                    int(neighbor_movie_id),
+                    self.input_id_kind,
+                    score
+                ])
 
         self.save_recommendations_to_csv(recommendations)
